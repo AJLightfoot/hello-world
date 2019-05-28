26f0b692c268e479efdeb89564fd58eb94e0485e
AJ Lightfoot <50601777+ajlightfoot@users.noreply.github.com>
2019-05-27 19:09:48 -0700

    CH8 - TIY exercises from Book 8-7

---
+++ CH8-Album.py
@@ -0,0 +1,14 @@
+def make_album(artist_nm, album_nm, track_nm=''):
+    album_info = {'artist': artist_nm, 'album': album_nm}
+    if track_nm:
+        album_info['track_nm'] = track_nm
+    return album_info
+
+album_data = make_album('bathory', 'hammerheart', '8')
+print(album_data)
+
+album_data = make_album('genghis tron', 'dead mountain mouth')
+print(album_data)
+
+album_data = make_album('the midnight', 'endless summer', '12')
+print(album_data)
