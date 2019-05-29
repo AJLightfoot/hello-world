8c040cd846c4dc33d70002cfb3c29b02105e863b
AJLightfoot <ajlightfootog@gmail.com>
2019-05-29 11:40:28 -0700

    CH8 - modifying a list using a function exercise

---
+++ CH8-Printing-Models.py
@@ -0,0 +1,18 @@
+#modifying a list in a function
+#start with some designs that need to be printed.
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+#simulate printing each design, until none are left.
+# Move each design to completed_models after printing.
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+
+    #simulate creating a 3D print from the design.
+    print("Printing model: " + current_design)
+    completed_models.append(current_design)
+
+#display all completed models.
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
