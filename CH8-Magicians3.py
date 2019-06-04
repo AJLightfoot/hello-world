a51a75113fce08da4cf45639605cd0e98284d441
AJ Lightfoot <50601777+ajlightfoot@users.noreply.github.com>
2019-06-03 19:17:05 -0700

    CH8-Appending list and showing revisions - showing appended list and original list using a slice

--- CH8-Magicians3.py
+++ CH8-Magicians3.py
@@ -3,18 +3,21 @@ def show_mages(mages):
         print(mage)
 
 def make_great(mages):
+
     great_mages = []
+
     while mages:
         mage = mages.pop()
         great_mage = mage + ' the Great'
         great_mages.append(great_mage)
+
     for great_mage in great_mages:
         mages.append(great_mage)
-        print(great_mage)
 
     return mage_list
 
 mage_list = ['bigby', 'mordenkainen', 'tenser', 'leomund', 'otiluke']
+show_mages(mage_list)
 
 print("\nGreat mages:")
 great_mages = make_great(mage_list[:])
